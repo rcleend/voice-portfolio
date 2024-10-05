25a86ffb1fd3d65361cdd332912c6df7c067f1da
Roel Leenders <roel@getluna.ai>
2024-10-05 17:47:04 +0200

    Fix build

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -6,8 +6,10 @@ export function Avatar({
 }: ReturnType<typeof useMarvinAvatar>) {
   return (
     <div className="relative">
-      {/* <Image src={avatarSrc} alt="Marvin Avatar" width={48} height={48} /> */}
-      <div className="w-10 h-10 bg-blue-500">{state}</div>
+      <img src={avatarSrc} alt="Marvin Avatar" width={48} height={48} />
+      <div className="absolute bottom-0 right-0 w-4 h-4 rounded-full bg-blue-500">
+        {state}
+      </div>
     </div>
   );
 }
